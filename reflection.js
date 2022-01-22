4d34ef1f4183d9fbef30b64f5f90564b622b660a
Mohd Ali Bin Naser <mohdalibin2001@gmail.com>
2022-01-22 04:19:43 +0300

    Linked the vanilla-tilt.js file and added code

---
+++ reflection.js
@@ -0,0 +1,10 @@
+
+// This file contains the JavaScript Code for the "Hover Reflection Effect" Project.
+
+// The code below initializes an instant of the VanillaTilt object on the container of the main image.
+VanillaTilt.init(document.querySelector(".iamge-container"), {
+
+    max: 25,
+    speed: 400
+
+})
